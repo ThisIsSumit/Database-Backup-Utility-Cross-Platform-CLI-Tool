de816e0380801523ecd99d3a6d697647b85a2752
ThisIsSumit <sumitkumar453827@gmail.com>
2025-12-15 17:54:29 +0530

    project added

---
+++ src/main/java/com/databasebackuputility/DatabaseBackupUtilityApplication.java
@@ -0,0 +1,13 @@
+package com.databasebackuputility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseBackupUtilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseBackupUtilityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/databasebackuputility/DatabaseBackupUtilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.databasebackuputility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseBackupUtilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
